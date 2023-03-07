bbd1ae567bd89aae6d54382a3ebbb4a6f4b8ca11
TheLonelyDude <magnus.kongstad@gmail.com>
2023-03-07 19:24:40 +0100

    Update ServletDeleteUser.java

--- demoa/src/main/java/com/example/demoa/ServletDeleteUser.java
+++ demoa/src/main/java/com/example/demoa/ServletDeleteUser.java
@@ -1,15 +1,22 @@
 package com.example.demoa;
 
+import com.example.demoa.Entity.User;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
-
+import java.util.Map;
+@SuppressWarnings("unchecked")
 @WebServlet(name = "ServletDeleteUser", value = "/ServletDeleteUser")
 public class ServletDeleteUser extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        if (getServletContext().getAttribute("users") instanceof Map) {
+            Map<String, User> Users = (Map<String, User>) getServletContext().getAttribute("users");
+            String user = request.getParameter("username");
 
+        }
     }
 
     @Override
